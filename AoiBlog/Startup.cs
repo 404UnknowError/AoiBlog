e41d4c72476d69e04a8935cde81637b0f230d83a
huangxuyang <1060236893@qq.com>
2020-08-10 09:47:00 +0800

    1.初始化项目

---
+++ AoiBlog/Startup.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using VueCliMiddleware;
+
+namespace AoiBlog
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        /// <summary>
+        /// 记录sql语句日志
+        /// </summary>
+        public static readonly ILoggerFactory MyLoggerFactory
+             = LoggerFactory.Create(builder =>
+             {
+
+                 builder.AddFilter((category, level) =>
+                   category == DbLoggerCategory.Database.Command.Name
+                   && level == LogLevel.Information).AddConsole();
+
+             });
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddSpaStaticFiles(option => option.RootPath = "clientapp/dist");
+
+            services.AddDbContext<AoiBlogModel.AoiBlogEntity>(options => options.UseMySql(Configuration.GetConnectionString("main")).UseLoggerFactory(MyLoggerFactory));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env,AoiBlogModel.AoiBlogEntity mainContext)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+            app.UseSpaStaticFiles();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            #region 数据库操作
+
+            
+            #endregion
+        }
+    }
+}
---
+++ AoiBlogModel/AoiBlogEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace AoiBlogModel
+{
+    public class AoiBlogEntity:DbContext
+    {
+        public AoiBlogEntity(DbContextOptions<AoiBlogEntity> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+       
+    }
+}
